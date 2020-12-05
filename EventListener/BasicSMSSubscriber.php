49ab15af793e7d278a17ff98765c2a84567f4df8
Bas Tolen <bas@gopublic.nl>
2020-12-05 12:42:28 +0100

    Update

--- EventListener/BasicSMSSubscriber.php
+++ EventListener/BasicSMSSubscriber.php
@@ -35,8 +35,11 @@ class BasicSMSSubscriber extends CommonSubscriber
     if ($event->checkContext(self::ACTION)) {
       $config = $event->getConfig();
       $message = $config['message'];
+      $change_lang_code = $config['change_lang_code'];
+      $default_lang_code = isset($config['default_lang_code']) ? $config['default_lang_code'] : '';
+      $contact_number_field = $config['contact_number_field'];
       try {
-        $this->messageService->sendMessage($message, $event->getLead());
+        $this->messageService->sendMessage($contact_number_field, $message, $change_lang_code, $default_lang_code, $event->getLead());
         $event->setResult(true);
       } catch (\Exception $e) {
         $event->setFailed($e->getMessage());
